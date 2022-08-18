d6642080b66b1c9f4040179f683a6db020423d6c
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2022-08-17 21:22:56 -0400

    Added 'AuthContext'

---
+++ src/context/AuthContext.js
@@ -0,0 +1,53 @@
+import { useContext, createContext, useEffect, useState } from 'react';
+import {
+    GoogleAuthProvider,
+    signInWithRedirect,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+} from 'firebase/auth';
+import { auth } from '../firebase';
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState({});
+
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        return signInWithRedirect(auth, provider)
+    };
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log('User', currentUser)
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{createUser, user, signIn, googleSignIn, logOut}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const UserAuth = () => {
+    return useContext(AuthContext);
+};
