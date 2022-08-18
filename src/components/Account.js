bf96298e27a03255a22dd7df2856e00b36daaeb6
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2022-08-17 21:24:33 -0400

    Added 'SignupPage', 'NavbarTop', 'Account' and 'Protected' files

---
+++ src/components/Account.js
@@ -0,0 +1,17 @@
+import { React } from 'react';
+import { UserAuth } from '../context/AuthContext';
+
+const Account = () => {
+    const { user } = UserAuth();
+
+    return (
+        <div>
+            <h1>Account</h1>
+            <div>
+                <p>Welcome, {user?.displayName ? user.displayName : 'User' }</p>
+            </div>
+        </div>
+    );
+};
+
+export default Account;
---
+++ src/components/NavbarTop.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
+const NavbarTop = () => {
+    const { user, logOut } = UserAuth();
+
+    const handleSignOut = async () => {
+        try {
+            await logOut()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            {user ? (
+                <button onClick={handleSignOut}
+                >Logout</button>
+            ) : (
+                <Link to='/login'>Login</Link>
+            )}
+        </div>
+    );
+};
+
+export default NavbarTop;
