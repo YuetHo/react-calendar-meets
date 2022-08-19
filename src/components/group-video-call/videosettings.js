44ed945796572bd89df38f2a3c00688164e5e2ae
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2022-08-19 18:17:34 -0400

    Token expired so added newly created token

--- src/components/group-video-call/videosettings.js
+++ src/components/group-video-call/videosettings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "03a39b9a96434b6f9a647738468ab530";
 const token =
-    "007eJxTYHi77qTK3E8nhFdoTVPawy95+IOwUzVXNkf+2Scxi3g15xQrMBgYJxpbJlkmWpqZGJskmaVZJpqZmJsbW5iYWSQmmRobXJj+L8n/5f+kWXN/sDAyQCCIz8KQm5iZx8AAAGOlIus=";
+    "007eJxTYBBdtGrLBX5BZ7dOk2zhV2s3u5xe8P5E9hPRi3n95/r38YgpMBgYJxpbJlkmWpqZGJskmaVZJpqZmJsbW5iYWSQmmRobaHEzJK+MYUxmPqnBysgAgSA+C0NuYmYeAwMAGUseRw==";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
