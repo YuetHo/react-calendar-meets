0ef407e78c81600f4dc85f59ad40452d076741dc
YuetHo <yhe944@gmail.com>
2022-08-20 01:14:36 -0400

    Added localStorage to calendar events

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import format from 'date-fns/format';
 import getDay from 'date-fns/getDay';
 import parse from 'date-fns/parse';
@@ -21,53 +21,72 @@ const localizer = dateFnsLocalizer({
   locales
 })
 
-// example events, remove this later
+
 const events = [
-  
+
 ];
+
 function CalendarPage() {
-  const [newEvent, setNewEvent] = useState({title: "" , start: "", end: ""})
-  const [allEvents, setAllEvents] = useState(events)
+  
+  // eventsToLoad is the default state for allEvents
+  // if the localstorage for 'events' exist then it will load that saved state into allEvents
+  // else it will just use the empty events[] defined above, loading no events on the calendar
+  const eventsToLoad = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : events
+
+  // calendar events are in allEvents state [], THERE IS CURRENTLY NO WAY TO DELETE EVENTS THROUGH THE UI
+  const [allEvents, setAllEvents] = useState(eventsToLoad)  
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" })
 
   function handleAddEvent() {
-    setAllEvents([...allEvents, newEvent])
+    if (newEvent.title === "" || newEvent.start === "" || newEvent.end === "") {
+      console.log('Not enough event info')
+    }
+    else {
+      setAllEvents([...allEvents, newEvent])
+    }
   }
+
+  // whenever list of events is updated, save that to localstorage, this allows the events to persist on page reload
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(allEvents));
+  }, [allEvents]);
+
   return (
     <div className="App">
-      <h1> Calendar</h1>
-      <h2> Add new events</h2>
+      <button> Add Event</button>
       <div>
-        <input 
-          type="text" 
-          placeholder="Add Title" 
-          style={{width: "20%" , marginRight: "10px"}}
-          value={newEvent.title} 
-          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value})}
-          />
+        <input
+          type="text"
+          placeholder="Add Title"
+          style={{ width: "20%", marginRight: "10px" }}
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
 
-        <DatePicker placeholderText="Start Date" 
-          style={{marginRight: "10px"}}
-          selected={newEvent.start} 
-          onChange={(start) => setNewEvent({...newEvent, start})} />
-        <DatePicker 
-          laceholderText="End Date"
-          selected={newEvent.end} 
-          onChange={ (end) => setNewEvent({...newEvent, end})} />
-         
-         <button style={{marginTop: "10px" }} onClick= {handleAddEvent} > 
-          Add Event 
-         </button>
-            
+        <DatePicker
+          placeholderText="Start Date"
+          style={{ marginRight: "10px" }}
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })} />
+
+        <DatePicker
+          placeholderText="End Date"
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })} />
+
+        <button style={{ marginTop: "10px" }} onClick={handleAddEvent} >
+          Submit
+        </button>
+
       </div>
 
-    <Calendar 
-      localizer={localizer} 
-      events={allEvents}
-      startAccessor = "start"
-      endAccessor="end"
-      style={{height: 500 ,margin:"50px"}}
-
-    />
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+      />
 
     </div>
   );
